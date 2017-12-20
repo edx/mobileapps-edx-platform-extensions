f5abddaee1009ec95e93ce0e72711b2034e17948
Aamish Baloch <aamish.baloch@gmail.com>
2017-12-20 15:21:43 +0500

    YONK-837: Colors attributes added in organization themes

---
+++ mobileapps/migrations/0005_auto_20171219_0647.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mobileapps', '0004_auto_20171128_0541'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='theme',
+            name='completed_course_tint',
+            field=models.CharField(max_length=255, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='theme',
+            name='header_background_color',
+            field=models.CharField(max_length=255, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='theme',
+            name='lesson_navigation_color',
+            field=models.CharField(max_length=255, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='theme',
+            name='navigation_icon_color',
+            field=models.CharField(max_length=255, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='theme',
+            name='navigation_text_color',
+            field=models.CharField(max_length=255, null=True, blank=True),
+        ),
+    ]
